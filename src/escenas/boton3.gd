cce59118a9247604794452ac2595a7ddc2f46433
Garcia-IGC <ignacio.garcia02@alumnos.ucn.cl>
2025-11-02 23:04:30 -0300

    controladores manuales de cinta para pruebas

---
+++ src/escenas/boton3.gd
@@ -0,0 +1,6 @@
+extends StaticBody3D
+
+func _input_event(camera, event, position, normal, shape_idx):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		print("Click en el objeto 3D!")
+		get_node("../Cinta2").belt_speed = -0.04
